64af329deb95daed373743a12ef0b11d11be0b5d
Neil Bateman and Tae Lee <neil@>
2019-09-03 16:37:12 -0700

    It works with today homework.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
+import { Switch, Route } from 'react-router-dom';
+import NewTicketForm from './NewTicketForm';
+
+function App(){
+  return (
+    <div>
+    <Header/>
+    <Switch>
+      <Route exact path='/' component={TicketList} />
+      <Route path='/newticket' component={NewTicketForm} />
+    </Switch>
+    </div>
+  );
+}
+
+export default App;
+
+{/* import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
 import MyStyledComponent from './MyStyledComponent';
 import Example from "./Example";
+import { Switch, Route } from 'react-router-dom';
 
 function App(){
   return (
@@ -16,4 +37,4 @@ function App(){
   );
 }
 
-export default App;
+export default App; */}
